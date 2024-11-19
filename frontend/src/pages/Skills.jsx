8e98b21fad5bc2a828f0be85abd8ae7167a54071
Maniteja Gurenka <140676670+manitejaraogurenka@users.noreply.github.com>
2024-11-19 07:19:42 +0530

    Update Skills.jsx

--- frontend/src/pages/Skills.jsx
+++ frontend/src/pages/Skills.jsx
@@ -163,7 +163,7 @@ const getSkillImage = (key) => {
     case 5:
       return "https://raw.githubusercontent.com/reduxjs/redux/master/logo/logo.png";
     case 6:
-      return "https://camo.githubusercontent.com/9771a2d4a7366d3c6d4793e17104eba9e88f0aec82f7165bfe6871455c26cb2c/68747470733a2f2f6173736574732e76657263656c2e636f6d2f696d6167652f75706c6f61642f76313636323133303535392f6e6578746a732f49636f6e5f6461726b5f6261636b67726f756e642e706e67";
+      return "https://nextjs.org/favicon.ico";
     case 7:
       return "https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg";
     case 8:
